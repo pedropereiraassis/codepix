2c2acc572bd1fc0fea6ad362a1be17853fcf0554
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-10-17 18:51:34 -0300

    chore: add db file

---
+++ infrastructure/db/db.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pedropereiraassis/codepix/domain/model"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	_ "gorm.io/driver/sqlite"
+)
+
+func init() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+
+	var err error
+
+	if os.Getenv("ENV") == "docker" {
+		err = godotenv.Load(basepath + "/../../.env.docker")
+	} else {
+		err = godotenv.Load(basepath + "/../../.env")
+	}
+
+	if err != nil {
+		log.Fatalf("Error loading .env files")
+	}
+}
+
+func ConnectDB(env string) *gorm.DB {
+	var dsn string
+	var db *gorm.DB
+	var err error
+
+	if env != "test" {
+		dsn = os.Getenv("DSN")
+		db, err = gorm.Open(os.Getenv("DB_TYPE"), dsn)
+	} else {
+		dsn = os.Getenv("DSN_TEST")
+		db, err = gorm.Open(os.Getenv("DB_TYPE_TEST"), dsn)
+	}
+
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+		panic(err)
+	}
+
+	if os.Getenv("DEBUG") == "true" {
+		db.LogMode(true)
+	}
+
+	if os.Getenv("AUTO_MIGRATE_DB") == "true" {
+		db.AutoMigrate(&model.Bank{}, &model.Account{}, &model.PixKey{}, &model.Transaction{})
+	}
+
+	return db
+}
