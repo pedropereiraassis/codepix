bb65d92fbec6e4da243c57e32d69bb1f522aa8d9
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-10-20 18:38:29 -0300

    feat: update pix-keys resource

--- nestjs/src/pix-keys/pix-keys.service.ts
+++ nestjs/src/pix-keys/pix-keys.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePixKeyDto } from './dto/create-pix-key.dto';
 import { UpdatePixKeyDto } from './dto/update-pix-key.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PixKey } from './entities/pix-key.entity';
+import { Repository } from 'typeorm';
+import { BankAccount } from 'src/bank-accounts/entities/bank-account.entity';
 
 @Injectable()
 export class PixKeysService {
-  create(createPixKeyDto: CreatePixKeyDto) {
-    return 'This action adds a new pixKey';
+
+  constructor(
+    @InjectRepository(PixKey) private pixKeyRepo: Repository<PixKey>,
+    @InjectRepository(BankAccount) private bankAccountRepo: Repository<BankAccount>,
+  ) { }
+
+  async create(bankAccountId: string, createPixKeyDto: CreatePixKeyDto) {
+    await this.bankAccountRepo.findOneOrFail({
+      where: { id: bankAccountId },
+    });
+
+    return await this.pixKeyRepo.save({
+      bank_account_id: bankAccountId,
+      ...createPixKeyDto,
+    })
   }
 
-  findAll() {
-    return `This action returns all pixKeys`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} pixKey`;
-  }
-
-  update(id: number, updatePixKeyDto: UpdatePixKeyDto) {
-    return `This action updates a #${id} pixKey`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} pixKey`;
+  async findAll(bankAccountId: string) {
+    return await this.pixKeyRepo.find({
+      where: { bank_account_id: bankAccountId },
+      order: { created_at: 'DESC' },
+    });
   }
 }
