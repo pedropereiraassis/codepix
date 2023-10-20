e12f9f8d56c8fdf9c0347817d7b327b80535e558
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-10-20 16:11:20 -0300

    chore: add typeORM & db connection configs

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'nest',
+      username: 'postgres',
+      password: 'root',
+      entities: [BankAccount],
+      synchronize: true,
+    }),
+    BankAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
