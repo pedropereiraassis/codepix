53438e4fd54ecb8dc795437e65df449cae7ee119
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-10-23 14:14:27 -0300

    feat: add pix-keys grpc interface file

---
+++ nestjs/src/pix-keys/pix-keys.grpc.ts
@@ -0,0 +1,31 @@
+import { Observable } from "rxjs"; // reactive x
+
+export interface Account {
+  accountId: string;
+  accountNumber: string;
+  bankId: string;
+  bankName: string;
+  ownerName: string;
+  createdAt: string;
+}
+
+export interface RegisterPixKeyRpcResponse {
+  id: string;
+  kind: string;
+  key: string;
+  account: Account;
+  createdAt: string;
+}
+
+export interface PixKeyClientGrpc {
+  registerPixKey: (data: {
+    kind: string;
+    key: string;
+    accountId: string;
+  }) => Observable<{ id: string; status: string; error: string }>;
+
+  find: (data: {
+    kind: string;
+    key: string;
+  }) => Observable<RegisterPixKeyRpcResponse>
+}
